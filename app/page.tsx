a4ac41f4eb86fbc12b4d0d9b8b1799e5a952c9d1
victor-kofa <victor.owusu@kofa.co>
2025-09-09 14:24:45 +0000

    import and use spinwheel

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import SpinningWheel from "@/components/SpinningWheel";
+
 export default function Home() {
   return (
-    <section className="min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold mb-4">
-        Welcome to the Engineering Dashboard
-      </h1>
-    </section>
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold mb-8">Welcome to Our Service!</h1>
+      <p className="text-lg mb-12">
+        Spin the wheel before you can access the dashboard
+      </p>
+
+      <SpinningWheel />
+    </div>
   );
 }
